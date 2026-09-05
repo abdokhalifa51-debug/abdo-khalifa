35b8c614c2e0e7855dc3c2eff4d95d2fbafd61ea
abdokhalifa51-debug <abdokhalifa51@gmail.com>
2026-09-05 16:04:27 +0300

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,33 +1,40 @@
 package com.alrayan.motostock;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.webkit.WebChromeClient;
-import android.webkit.JavascriptInterface;
-import android.content.Context;
-import android.view.Window;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
-    WebView webView;
-    @Override public void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         webView = new WebView(this);
-        WebSettings s = webView.getSettings();
-        s.setJavaScriptEnabled(true);
-        s.setDomStorageEnabled(true);
-        s.setAllowFileAccess(true);
-        s.setAllowContentAccess(true);
-        s.setBuiltInZoomControls(false);
-        s.setDisplayZoomControls(false);
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.addJavascriptInterface(new AndroidBridge(this), "Android");
         setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        
+        // تحميل الصفحة الرئيسية بدون إعادة تحميل مستمرة تسبب الرعشة
         webView.loadUrl("file:///android_asset/index.html");
     }
-    @Override public void onBackPressed() { if (webView.canGoBack()) webView.goBack(); else super.onBackPressed(); }
-    public static class AndroidBridge { Context c; AndroidBridge(Context c){this.c=c;} @JavascriptInterface public String version(){return "Android 1.0";} }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
